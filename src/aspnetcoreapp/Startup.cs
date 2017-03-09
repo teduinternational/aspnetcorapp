38a1266de612e4b7dff81a0c71754638f97f63be
Toanbn <ngoctoan89@gmail.com>
2017-03-09 21:51:09 +0700

    Initial lesson 5

---
+++ src/aspnetcoreapp/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using aspnetcoreapp.Milddewares;
+using System.Globalization;
+
+namespace aspnetcoreapp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseRequestCulture();
+            app.UseWelcomePage();
+            // loggerFactory.AddConsole();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                var cultureQuery = context.Request.Query["culture"];
+                if (!string.IsNullOrWhiteSpace(cultureQuery))
+                {
+                    var culture = new CultureInfo(cultureQuery);
+
+                    CultureInfo.CurrentCulture = culture;
+                    CultureInfo.CurrentUICulture = culture;
+
+                }
+
+                // Call the next delegate/middleware in the pipeline
+               // return this._next(context);
+                await context.Response.WriteAsync("Hello World!");
+            });
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
+        }
+    }
+}
