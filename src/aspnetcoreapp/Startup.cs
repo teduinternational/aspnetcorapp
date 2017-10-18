7090ae3b55d28089310f3bf9810a596b5130d7ba
Toanbn <ngoctoan89@gmail.com>
2017-10-18 09:21:34 +0700

    migration to 2.0

--- src/aspnetcoreapp/Startup.cs
+++ src/aspnetcoreapp/Startup.cs
@@ -5,11 +5,29 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using aspnetcoreapp.Milddewares;
 using System.Globalization;
+using Microsoft.Extensions.Configuration;
 
 namespace aspnetcoreapp
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            //if (env.IsDevelopment())
+            //{
+            //    builder.AddUserSecrets<Startup>();
+            //}
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
