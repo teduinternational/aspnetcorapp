4e294747393fcf492ea67b70de5e2c8849439c22
Toanbn <ngoctoan89@gmail.com>
2017-10-24 23:56:58 +0700

    lesson 9: Dependency injection

--- src/aspnetcoreapp/Controllers/HomeController.cs
+++ src/aspnetcoreapp/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using aspnetcoreapp.DI.Implementation;
+using aspnetcoreapp.DI.Interfaces;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,22 @@ namespace aspnetcoreapp.Controllers
 {
     public class HomeController : Controller
     {
+        ICustomerRepository _customerRepository;
+        public HomeController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            //Service locator
+            //var repository = HttpContext.RequestServices.GetService(typeof(ICustomerRepository));
+
+            _customerRepository.Add(new Models.Customer() { Id = 1, Name = "Nguyen Van A" });
+            _customerRepository.Save();
+
+            var customers = _customerRepository.GetAll();
+            return View(customers);
         }
     }
 }
--- src/aspnetcoreapp/Startup.cs
+++ src/aspnetcoreapp/Startup.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.Logging;
 using aspnetcoreapp.Milddewares;
 using System.Globalization;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using aspnetcoreapp.Models;
+using aspnetcoreapp.DI.Interfaces;
+using aspnetcoreapp.DI.Implementation;
 
 namespace aspnetcoreapp
 {
@@ -32,6 +36,10 @@ namespace aspnetcoreapp
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+              options.UseInMemoryDatabase("CoreApp")
+            );
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
             services.AddMvc();
         }
 
@@ -39,29 +47,29 @@ namespace aspnetcoreapp
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseRequestCulture();
-            app.UseWelcomePage();
+            //app.UseWelcomePage();
             // loggerFactory.AddConsole();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                var cultureQuery = context.Request.Query["culture"];
-                if (!string.IsNullOrWhiteSpace(cultureQuery))
-                {
-                    var culture = new CultureInfo(cultureQuery);
+            //app.Run(async (context) =>
+            //{
+            //    var cultureQuery = context.Request.Query["culture"];
+            //    if (!string.IsNullOrWhiteSpace(cultureQuery))
+            //    {
+            //        var culture = new CultureInfo(cultureQuery);
 
-                    CultureInfo.CurrentCulture = culture;
-                    CultureInfo.CurrentUICulture = culture;
+            //        CultureInfo.CurrentCulture = culture;
+            //        CultureInfo.CurrentUICulture = culture;
 
-                }
+            //    }
 
-                // Call the next delegate/middleware in the pipeline
-               // return this._next(context);
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //    // Call the next delegate/middleware in the pipeline
+            //   // return this._next(context);
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
             app.UseMvcWithDefaultRoute();
             app.UseStaticFiles();
         }
